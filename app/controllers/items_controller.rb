1355f5994cc22564aa4cb90e08d09e5948285c35
karimaazib <azib.karima@gmail.com>
2020-11-23 20:09:30 +0100

    first

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,24 @@
+class ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
